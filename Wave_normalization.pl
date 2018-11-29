508103627b6de23c82af36d67b0b73bde3e1e03a
ngrainger <45470768+ngrainger@users.noreply.github.com>
2018-11-29 14:19:13 -0800

    Add files via upload

---
+++ Wave_normalization.pl
@@ -0,0 +1,63 @@
+ #!/usr/bin/perl
+
+#use warnings;
+#use strict;
+
+use List::MoreUtils 'pairwise';
+use List::Util qw( min max );
+use File::Basename;
+
+#open (OUT, ">normalized_data.txt") or die "could not open OUT\n";
+
+foreach $a($ARGV[0]){ #each file given in argument line is read (can read multiple files at a time)
+
+	open (IN, "<$a") or die "couldn't open $a\n";
+	
+	
+	$a =~ /(\d+\w+\.+?)/;
+	my $info = ($a = basename($a));
+	open (OUT, ">calibrated_data_$info.txt") or die "could not open OUT\n";
+	my $ctr = ();
+	my @particlelength = ();
+	my $normalize = ();
+	
+	<IN>;
+	while(my $line = <IN>){ #reads IN into $line variable
+		my @column = split(',',$line); #splits line being read on space and places each into a column array
+		
+		push(@particlelength, $column[6]); #pushes column 3 to particlelength array
+		$ctr++; #adds to counter for every line	
+	}
+		
+	print OUT "File Name: $info\n";
+	print OUT "-------------------------------------\n\n";
+	print OUT "Total Number of Particles: $ctr\n";
+
+    
+    my $max = max @particlelength; 
+    print OUT "Normalized Ca2+ Wave Spread (% of max):\n";
+    print OUT "Longest Ca2+ Wave Travel Distance: $max\n";
+    print OUT "--------------------------------------------\n";
+    
+    foreach my $particlelength (@particlelength) {
+		my $normalize = $ARGV[1];
+		$particlelengthnormalized = (($particlelength/$normalize)*100);
+		$particlelengthnormalized = sprintf "%.2f", $particlelengthnormalized;
+    	print OUT "$particlelengthnormalized\n";
+    }
+
+    
+    print OUT "\n";
+    print OUT "Spatial Spread (um):\n";
+    print OUT "--------------------\n";
+    
+    foreach my $particlelength (@particlelength) {
+    	print OUT "$particlelength";
+    }
+
+}
+
+close (IN);
+close (OUT); 
+
+
